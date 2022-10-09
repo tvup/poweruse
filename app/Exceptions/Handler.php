2f856654267a859081e85218b965b2f7ef3a4ece
Torben Evald Hansen <tvupper@gmail.com>
2022-10-09 03:06:56 +0200

    add a warning, when 419 occurs and redirect to /el

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -28,6 +28,20 @@ class Handler extends ExceptionHandler
     ];
 
     /**
+     * Register the exception handling callbacks for the application.
+     *
+     * @return void
+     */
+    public function register($request)
+    {
+        $this->renderable(function (Throwable $e) use ($request) {
+            if ($e->getPrevious() instanceof \Illuminate\Session\TokenMismatchException) {
+                return redirect('el')->with('warning', 'Session udløbet - prøv igen')->withInput($request->all());;
+            };
+        });
+    }
+
+    /**
      * Report or log an exception.
      *
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
--- resources/views/el.blade.php
+++ resources/views/el.blade.php
@@ -7,6 +7,11 @@
             {{ session('status') }}
         </div>
     @endif
+    @if (session('warning'))
+        <div class="alert alert-warning">
+            {!! session('warning') !!}
+        </div>
+    @endif
     @if (session('error'))
         <div class="alert alert-danger">
             {!! session('error') !!}
