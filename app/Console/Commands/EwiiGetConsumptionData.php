044262b29a779c084a03718ffb3e87f294c08076
Torben Evald Hansen <tvupper@gmail.com>
2022-11-12 05:01:52 +0100

    Make sure inputs are of the correct type EwiiGetConsumptionData

--- app/Console/Commands/EwiiGetConsumptionData.php
+++ app/Console/Commands/EwiiGetConsumptionData.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Console\Commands\Traits\OutputApiExceptionMessages;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Validator;
 use Tvup\EwiiApi\EwiiApi;
 use Tvup\EwiiApi\EwiiApiException;
 use Tvup\EwiiApi\EwiiApiInterface;
@@ -31,10 +32,6 @@ class EwiiGetConsumptionData extends Command
      */
     private $ewiiApi;
     /**
-     * @var int|string
-     */
-    private $showCount;
-    /**
      * @var bool
      */
     private $showAll;
@@ -57,23 +54,47 @@ class EwiiGetConsumptionData extends Command
      */
     public function handle()
     {
-        $this->option('show-count');
-        $optionShowCount = $this->option('show-count');
+        $showCount = $this->option('show-count');
+
+        $rulesForShowCount = [
+            'string' => 'in:ALL',
+            'numeric' => 'integer'
+        ];
+
+        $rules = [
+            'show-count' => ['required', $rulesForShowCount[$this->getShowCountType($showCount)]],
+        ];
+
+        $validator = Validator::make([
+            'show-count' => $showCount,
+        ], $rules);
+
+        if ($validator->fails()) {
+            $this->info('eloverblik:get-meter-data was not run. See errors below:');
+
+            foreach ($validator->errors()->all() as $error) {
+                $this->error($error);
+            }
+            return 1;
+        }
+
+        $safeValues = $validator->validated();
+        $optionShowCount = $safeValues['show-count'];
+
         switch ($optionShowCount) {
             case is_numeric($optionShowCount):
-                $this->showCount = $optionShowCount;
                 break;
             case 'ALL':
                 $this->showAll = true;
                 break;
             default:
-                $this->error('Show-count should either be a number of \'ALL\'');
+                $this->error('Show-count should either be a number or \'ALL\'');
                 return 1;
         }
 
         /** @var EwiiApi $ewiiApi */
         $ewiiApi = $this->ewiiApi;
-        if($this->getOutput()->isVerbose()) {
+        if ($this->getOutput()->isVerbose()) {
             $ewiiApi->setDebug(true);
         }
 
@@ -92,14 +113,24 @@ class EwiiGetConsumptionData extends Command
             return 1;
         }
 
+        $outputTableArray = [];
+
         if ($response) {
-            foreach($response as $key => $value) {
-                $response[$key] = [array_keys($value)[0], array_values($value)[0]];
+            foreach ($response as $key => $value) {
+                $outputTableArray[] = [$key, $value];
             }
-            $this->table(['Time' , 'Value'], $this->showAll ? $response : array_slice($response, 0, $this->showCount));
+            $this->table(['Time', 'Value'], $this->showAll ? $outputTableArray : array_slice($outputTableArray, 0, $optionShowCount));
         }
 
     }
 
+    private function getShowCountType(mixed $showCount): string
+    {
+        if (filter_var($showCount, FILTER_VALIDATE_INT) !== false) {
+            return 'numeric';
+        } else {
+            return gettype($showCount);
+        }
+    }
 
 }
