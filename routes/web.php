41ca2539aececa1088dd1b5c7f4707aca206244c
Lars Schou <lars@billy.dk>
2022-11-19 23:32:48 +0100

    Add breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,20 +16,32 @@
 
 Route::get('/', function () {
     return redirect('el');
-})->name('home');
+});
 
-Route::get('el/', 'ElController@index')->name('el');
-Route::get('el-meteringpoint/', 'ElController@indexMeteringPoint')->name('el-meteringpoint');
-Route::get('el-charges/', 'ElController@indexCharges')->name('el-charges');
-Route::get('el-spotprices/', 'ElController@indexSpotprices')->name('el-spotprices');
-Route::get('consumption/', 'ElController@indexConsumption')->name('consumption');
-Route::get('el-totalprices/', 'ElController@indexTotalPrices')->name('el-totalprices');
-Route::get('el-custom/', 'ElController@indexCustomUsage')->name('el-custom');
-Route::post('processdata', 'ElController@processData');
-Route::post('getMeteringPointData', 'ElController@getMeteringPointData');
-Route::post('getChargesForWeb', 'ElController@getChargesForWeb');
-Route::post('getSpotprices', 'ElController@getSpotprices');
-Route::post('getConsumption', 'ElController@getConsumption');
-Route::post('getTotalPrices', 'ElController@getTotalPrices');
-Route::post('processcustom', 'ElController@processCustom');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('el/', 'ElController@index')->name('el');
+    Route::get('el-meteringpoint/', 'ElController@indexMeteringPoint')->name('el-meteringpoint');
+    Route::get('el-charges/', 'ElController@indexCharges')->name('el-charges');
+    Route::get('el-spotprices/', 'ElController@indexSpotprices')->name('el-spotprices');
+    Route::get('consumption/', 'ElController@indexConsumption')->name('consumption');
+    Route::get('el-totalprices/', 'ElController@indexTotalPrices')->name('el-totalprices');
+    Route::get('el-custom/', 'ElController@indexCustomUsage')->name('el-custom');
+    Route::post('processdata', 'ElController@processData');
+    Route::post('getMeteringPointData', 'ElController@getMeteringPointData');
+    Route::post('getChargesForWeb', 'ElController@getChargesForWeb');
+    Route::post('getSpotprices', 'ElController@getSpotprices');
+    Route::post('getConsumption', 'ElController@getConsumption');
+    Route::post('getTotalPrices', 'ElController@getTotalPrices');
+    Route::post('processcustom', 'ElController@processCustom');
+
+});
+
+require __DIR__.'/auth.php';
