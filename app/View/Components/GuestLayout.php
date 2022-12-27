c58e747cf0041b40373290be21bdf5b68576b8a0
Lars Schou <lars@billy.dk>
2022-12-27 22:53:25 +0100

    Nope

--- app/View/Components/GuestLayout.php
+++ app/View/Components/GuestLayout.php
@@ -7,11 +7,6 @@ use Illuminate\View\Component;
 
 class GuestLayout extends Component
 {
-    /**
-     * Get the view / contents that represents the component.
-     *
-     * @return \Illuminate\View\View
-     */
     public function render(): View
     {
         return view('layouts.guest');
