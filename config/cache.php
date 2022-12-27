b6de39a1f88f17ef17ac429d05b08ac4dd48a72f
Lars Schou <lars@billy.dk>
2022-12-27 22:29:48 +0100

    maybe

--- config/cache.php
+++ config/cache.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('CACHE_DRIVER', 'array'),
+    'default' => env('CACHE_DRIVER', 'file'),
 
     /*
     |--------------------------------------------------------------------------
