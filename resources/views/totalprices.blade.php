54c424cbcc070e5ba89f8497acd3f4e2ac966b0c
Torben Evald Hansen <tvupper@gmail.com>
2022-12-28 03:02:13 +0100

    Add a2hs

--- resources/views/totalprices.blade.php
+++ resources/views/totalprices.blade.php
@@ -92,3 +92,17 @@
         });
     </script>
 @endif
+
+<script>
+    if ('serviceWorker' in navigator) {
+        // Register a service worker hosted at the root of the
+        // site using the default scope.
+        navigator.serviceWorker.register('/js/service-worker.js').then(function(registration) {
+            console.log('Service worker registration succeeded:', registration);
+        }, /*catch*/ function(error) {
+            console.log('Service worker registration failed:', error);
+        });
+    } else {
+        console.log('Service workers are not supported.');
+    }
+</script>
