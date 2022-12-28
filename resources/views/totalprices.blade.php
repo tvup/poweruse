7da4a492911062ae6f29e4956e1387d6a8244f61
Torben Evald Hansen <tvupper@gmail.com>
2022-12-28 03:31:27 +0100

    Use asset() to get serviceworker.js

--- resources/views/totalprices.blade.php
+++ resources/views/totalprices.blade.php
@@ -97,7 +97,7 @@
     if ('serviceWorker' in navigator) {
         // Register a service worker hosted at the root of the
         // site using the default scope.
-        navigator.serviceWorker.register('/js/service-worker.js').then(function(registration) {
+        navigator.serviceWorker.register({{ asset('/js/service-worker.js') }} ).then(function(registration) {
             console.log('Service worker registration succeeded:', registration);
         }, /*catch*/ function(error) {
             console.log('Service worker registration failed:', error);
