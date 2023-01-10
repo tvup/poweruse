35f1d766a6192ff43e168b5a18f0125af38c8a80
Torben Evald Hansen <tvupper@gmail.com>
2023-01-10 03:30:31 +0100

    Corrected retrieval of option

--- app/Console/Commands/DatahubPriceListRemoveProblematicData.php
+++ app/Console/Commands/DatahubPriceListRemoveProblematicData.php
@@ -32,7 +32,8 @@ class DatahubPriceListRemoveProblematicData extends Command
         $count5790001089030=0;
         $count5790000610099=0;
         $count5790001088309=0;
-        if($this->options('simulate')) {
+        if($this->option('simulate')) {
+            $this->info('SIMULATION ONLY!');
             $count5790001089030 = DB::table('datahub_price_lists')->where('ChargeType', 'D03')->whereIn('ChargeTypeCode', ['A1 Rådig R', 'A1R D', 'A2 R D', 'A2 Rådig R', 'A200GWH R', 'B1 R D', 'B1 Rådig R', 'B2 Rådig R', 'B20GWh R D', 'B2D R'])->where('GLN_Number','5790001089030')->whereIn('ValidFrom', ['2022-12-01', '2022-01-01'])->where('Note','Rabat på nettarif N1 A/S')->where('description', 'like', 'Rabat på nettarif N1 A/S%')->count();
             $count5790000610099 = DB::table('datahub_price_lists')->where('ChargeType', 'D03')->where('ChargeTypeCode', 'TCL<100_02')->where('GLN_Number','5790000610099')->where('ValidFrom', '2022-10-01')->where('Note','Nettarif C time')->where('Description', 'Tarif, hvor aftagepunktet typisk er i 0,4 kV-nettet med timeaflæst måler.')->count();
             $count5790001088309 = DB::table('datahub_price_lists')->where('ChargeType', 'D03')->where('ChargeTypeCode', 'STR-NT-21')->where('GLN_Number','5790001088309')->where('ValidFrom', '2022-01-01')->where('Note','Net rådighedstarif C')->where('Description','Rådighedstarif, hvor aftagepunktet typisk er i 0,4 kV-nettet.')->count();
