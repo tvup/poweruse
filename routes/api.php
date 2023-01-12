a598a7373c6024751eddfac3c9f39b0a2407493e
Torben Evald Hansen <tvupper@gmail.com>
2023-01-12 09:17:38 +0100

    Add passport and protect api

--- routes/api.php
+++ routes/api.php
@@ -25,4 +25,6 @@ Route::get('el/charges/{refreshToken}', 'ElController@getCharges');
 Route::get('el/{start_date}/{end_date}/{price_area}/{refreshToken}', 'ElController@getFromDate');
 Route::get('el/{refreshToken}/delete', 'ElController@delete');
 
-Route::apiResources(['meteringPoint' => 'Api\MeteringPointController']);
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::apiResources(['meteringPoint' => 'Api\MeteringPointController']);
+});
