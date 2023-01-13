f4780762014f7462c8405b6e55b79e2e38cb6892
Lars Schou <lars@billy.dk>
2023-01-13 22:52:43 +0100

    Change ui to bootstrap

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,8 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
+                'resources/sass/app.scss',
                 'resources/js/app.js',
-                'resources/js/serviceworker.js',
             ],
             refresh: true,
         }),
