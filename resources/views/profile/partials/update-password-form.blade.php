640ddfcc9dfd756e310a14d892b23d89f90cbecc
Lars Schou <lars@billy.dk>
2023-01-14 22:15:35 +0100

    Update profile UI to use bootrap 5

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -1,43 +1,33 @@
 <section>
-    <header>
-        <h2>Update Password</h2>
-        <p>Ensure your account is using a long, random password to stay secure.</p>
-    </header>
-
     <form method="post" action="{{ route('password.update') }}" >
         @csrf
         @method('put')
-
-        <div>
-            <label for="current_password"> {{__('Current Password') }} </label>
-            <input id="current_password" name="current_password" type="password" class="form-control" autocomplete="current-password" />
-            <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
-        </div>
-
-        <div>
-            <label for="password"> {{__('New Password')}} </label>
-            <input id="password" name="password" type="password" class="form-control" autocomplete="new-password" />
-            <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
-        </div>
-
-        <div>
-            <label for="password_confirmation">{{__('Confirm Password')}} </label>
-            <input id="password_confirmation" name="password_confirmation" type="password" class="form-control" autocomplete="new-password" />
-            <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="form-group">
-            <button class="btn btn-primary">{{ __('Save') }}</button>
-
-            @if (session('status') === 'password-updated')
-                <p
-                    x-data="{ show: true }"
-                    x-show="show"
-                    x-transition
-                    x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600"
-                >Saved.</p>
-            @endif
+        <div class="container rounded bg-white mt-5 mb-5">
+            <div class="row">
+                <div class="col-md-3 border-right">
+                    <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="https://plumbr.io/app/uploads/2015/01/thread-lock.jpeg">
+                    </div>
+                </div>
+                <div class="col-md-5 border-right">
+                    <div class="p-3 py-5">
+                        <div class="row mt-2">
+                            @if (session('status') === 'password-updated')
+                                <div class="alert alert-success" role="alert">
+                                    Password updated.
+                                </div>
+                            @endif
+                            <div class="d-flex justify-content-between align-items-center mb-3">
+                                <h4 class="text-right">Change password</h4>
+                            </div>
+                            <div class="col-md-12"><label class="labels">Current password</label><input type="password" class="form-control" placeholder="current password" name="current_password" autocomplete="current-password"></div>
+                            <div class="col-md-12"><label class="labels">New password</label><input type="password" class="form-control" placeholder="new password" name="password"></div>
+                            <div class="col-md-12"><label class="labels">Confirm password</label><input type="password" class="form-control" placeholder="password confirmation" name="password_confirmation"></div>
+                        </div>
+                        <div class="mt-5 text-left"><button  class="btn btn-primary">{{ __('Save') }}</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </form>
 </section>
