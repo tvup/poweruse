6cda9d8cd578dda6a99ce61dedf3efa7f1626611
Torben Evald Hansen <contact@torbenit.dk>
2023-01-21 10:39:29 +0100

    Add phpdoc for facade

--- app/Services/Transformers/Facades/MeteringPointTransformer.php
+++ app/Services/Transformers/Facades/MeteringPointTransformer.php
@@ -4,6 +4,9 @@ namespace App\Services\Transformers\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static transform(array $response, string $string)
+ */
 class MeteringPointTransformer extends Facade
 {
     protected static function getFacadeAccessor(): string
