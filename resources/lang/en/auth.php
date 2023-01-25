ba108d66b68956df8511b15acf3c4bee99f3a423
Torben Evald Hansen <tvupper@gmail.com>
2023-01-25 23:53:31 +0100

    Develop (#96)
    * Feature/use local data if present (#87)
    
    * Rewrite how metering point is retrieved
    
    * Add gitignore and fixture
    
    * Remove old method to get metering point
    
    * transformer  facade
    
    * Get charges locally
    
    * Use Charges from DB if present
    
    
    * Make fields dynamic, so they only appear when it makes sense
    
    * Also take into account, that we might not have meteringpoint when querying ewii
    
    * When EWII is queried, a metering point with charges has to be saved to POWERUSE beforehand
    
    * Introduce Enum for source
    
    * New transformer-method for outputting and corrections after introducing enum
    
    * Make totalprices view nice
    
    * Install ide helper
    
    * PSR
    
    * Small improvements
    
    * Test
    
    * Test
    
    * return type
    
    * PSR
    
    * Common increment for translations and other small, non-critical stuff (#94)
    
    * Remove unsued files, duplicate entry in routes, rename form id and name
    
    * Add translations
    
    * Add more translations and put in refresh-token if user has it saved on profile (and hide field)
    
    * Move assets to resources and put them under the versioning of Vite
    
    * Add more translations
    
    * Remove unused js
    
    * Add some translations, make return-message a paginated one
    
    * Adds missing comma to translation file
    
    * Returns paginated result as <int,mixed>. Style
    
    * Don't take these changes at this point - moved to other PR
    
    * Don't take these changes at this point - moved to other PR -2
    
    * Remember NL
    
    * Don't take these changes at this point - moved to other PR -3
    
    * Change datepicker to flatpickr
    
    * Add locale to flatpickr
    
    * Add translation for headline in totalprices
    
    * Makes better reading for refresh_token variable added with view
    
    * Makes better reading for refresh_token variable added with view -2
    
    Co-authored-by: Lars Schou <lars@billy.dk>

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => __('These credentials do not match our records.'),
+    'throttle' => __('Too many login attempts. Please try again in :seconds seconds.'),
 
 ];
