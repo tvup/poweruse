3536cffcf9f5b8796284f4624e053310a80547bb
Torben Evald Hansen <contact@torbenit.dk>
2023-01-26 11:36:36 +0100

    Style

--- app/Console/Commands/DbImport.php
+++ app/Console/Commands/DbImport.php
@@ -33,19 +33,18 @@ class DbImport extends Command
         $csv_file = database_path('fixtures/poweruse_datahub_price_lists.csv');
         $csv_file2 = storage_path('poweruse_datahub_price_lists_null_handled.csv');
 
-
         if (file_exists($csv_file)) {
             $this->info('Importing CSV export from ' . $csv_file);
 
             //Replace text NULL with \N
-            exec('sed \'s/NULL/\\\\N/g\' '.$csv_file.' > '. $csv_file2);
+            exec('sed \'s/NULL/\\\\N/g\' ' . $csv_file . ' > ' . $csv_file2);
 
             //Load file
             exec(sprintf(
                 'mysql -h%s -u%s %s --local-infile %s -e "LOAD DATA LOCAL INFILE \'%s\'  INTO TABLE %s  FIELDS TERMINATED BY \',\' OPTIONALLY ENCLOSED BY \'\"\' LINES TERMINATED BY \'\\n\'"',
                 $mysql_host,
                 config($mysql_connection . '.username'),
-                config($mysql_connection . '.password') != '' ? '-p\''. config($mysql_connection . '.password') . '\'' : '',
+                config($mysql_connection . '.password') != '' ? '-p\'' . config($mysql_connection . '.password') . '\'' : '',
                 config($mysql_connection . '.database'),
                 $csv_file2,
                 'datahub_price_lists'
