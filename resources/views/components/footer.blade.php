382a0fa45b7d2c950f731fff8ed6687b52cfcd0a
Torben Evald Hansen <contact@torbenit.dk>
2023-01-26 06:23:58 +0100

    Added links to github

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,7 +3,7 @@
         <span></span>
     </div>
     <div class="copyright">
-        <p>{{ __('Developed and maintained by') }} <a href="#" class="link-secondary" target="_blank">Tvup & Negoziator</a></p>
+        <p>{{ __('Developed and maintained by') }} <a href="https://github.com/tvup" class="link-secondary" target="_blank">Tvup</a> & <a class="link-secondary" target="_blank" href="https://github.com/negoziator">Negoziator</a></p>
     </div>
     <div>
         <ul class="d-flex gap-3 list-unstyled">
