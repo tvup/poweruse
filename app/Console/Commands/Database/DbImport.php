616de1eec8a07881482f92b294c32acec674f53c
Torben Evald Hansen <contact@torbenit.dk>
2023-01-28 02:43:24 +0100

    Make it possible to import every file in database/fixtures

--- app/Console/Commands/Database/DbImport.php
+++ app/Console/Commands/Database/DbImport.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands\Database;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 
 class DbImport extends Command
 {
@@ -30,25 +32,37 @@ class DbImport extends Command
         $mysql_connection = 'database.connections.mysql';
 
         $mysql_host = config('database.connections.mysql.host');
-        $csv_file = database_path('fixtures/poweruse_datahub_price_lists.csv');
-        $csv_file2 = storage_path('poweruse_datahub_price_lists_null_handled.csv');
 
-        if (file_exists($csv_file)) {
-            $this->info('Importing CSV export from ' . $csv_file);
+        $filesInFolder = File::files(database_path('fixtures'));
+        foreach ($filesInFolder as $path) {
+            $pathInfo = pathinfo($path);
+            $csv_file = database_path('fixtures/' . $pathInfo['basename']);
+            $csv_file2 = storage_path($pathInfo['filename'] . '_null_handled.csv');
 
-            //Replace text NULL with \N
-            exec('sed \'s/NULL/\\\\N/g\' ' . $csv_file . ' > ' . $csv_file2);
+            if (file_exists(database_path('fixtures/' . $pathInfo['filename'] . '.csv'))) {
+                $dataMeta = explode('-', $pathInfo['filename']);
+                if (count($dataMeta) <= 1) {
+                    continue;
+                }
+                $tableName = $dataMeta[1];
+                $this->info('Truncating ' . $tableName);
+                DB::table($tableName)->truncate();
+                $this->info('Importing CSV export from ' . $csv_file . ' to ' . $tableName);
 
-            //Load file
-            exec(sprintf(
-                'mysql -h%s -u%s %s --local-infile %s -e "LOAD DATA LOCAL INFILE \'%s\'  INTO TABLE %s  FIELDS TERMINATED BY \',\' OPTIONALLY ENCLOSED BY \'\"\' LINES TERMINATED BY \'\\n\'"',
-                $mysql_host,
-                config($mysql_connection . '.username'),
-                config($mysql_connection . '.password') != '' ? '-p\'' . config($mysql_connection . '.password') . '\'' : '',
-                config($mysql_connection . '.database'),
-                $csv_file2,
-                'datahub_price_lists'
-            ));
+                //Replace text NULL with \N
+                exec('sed \'s/NULL/\\\\N/g\' ' . $csv_file . ' > ' . $csv_file2);
+
+                //Load file
+                exec(sprintf(
+                    'mysql -h%s -u%s %s --local-infile %s -e "LOAD DATA LOCAL INFILE \'%s\'  INTO TABLE %s  FIELDS TERMINATED BY \',\' OPTIONALLY ENCLOSED BY \'\"\' LINES TERMINATED BY \'\\n\'"',
+                    $mysql_host,
+                    config($mysql_connection . '.username'),
+                    config($mysql_connection . '.password') != '' ? '-p\'' . config($mysql_connection . '.password') . '\'' : '',
+                    config($mysql_connection . '.database'),
+                    $csv_file2,
+                    $tableName
+                ));
+            }
         }
 
         return Command::SUCCESS;
