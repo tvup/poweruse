3bb0838457aa4c761d3336f0ceb82cfdfb2e6ca0
Torben Evald Hansen <contact@torbenit.dk>
2023-01-29 01:01:25 +0100

    Setup for running service-worker at /public

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Foundation\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,6 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        app()->make(Vite::class)->withEntryPoints(['resources/sass/app.scss', 'resources/js/app.js'])->useBuildDirectory('');
     }
 
     /**
