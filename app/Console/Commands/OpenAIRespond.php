3873d636e715214377ecf12473fb72ee80eab5c5
Torben Evald Hansen <tvupper@gmail.com>
2023-02-08 13:56:56 +0100

    Refactors OpenAI-command

--- app/Console/Commands/OpenAIRespond.php
+++ app/Console/Commands/OpenAIRespond.php
@@ -2,10 +2,12 @@
 
 namespace App\Console\Commands;
 
+use App\Services\SourceCodeService;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Console\Command;
 use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Filesystem\FilesystemAdapter;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use League\Flysystem\FilesystemException;
@@ -28,39 +30,38 @@ class OpenAIRespond extends Command
      */
     protected $description = 'Asks OPENAI your questions and streams back the response';
 
+    private SourceCodeService $sourceCodeService;
+
+    /**
+     * @var ConsoleOutput
+     */
+    private ConsoleOutput $consoleOutput;
+
     /**
      * Execute the console command.
      *
+     * @param SourceCodeService $sourceCodeService
      * @return int
      * @throws BindingResolutionException
      */
-    public function handle(): int
+    public function handle(SourceCodeService $sourceCodeService): int
     {
+        $this->sourceCodeService = $sourceCodeService;
+        $this->consoleOutput = app()->make(ConsoleOutput::class);
         stream_filter_register('remove_all_the_open_a_i_noise_from_stream_filter', 'App\StreamUtilities\RemoveAllTheOpenAINoiseFromStreamFilter');
 
         $question = implode(' ', $this->argument()['question']);
-
-        $output = new ConsoleOutput();
 
         $fileContent = '';
 
         $file = $this->option('file');
 
         if ($file) {
-            try {
-                $fileContent = Storage::disk('src')->read($file);
-            } catch (FilesystemException $e) {
-                $output->writeln('It wasn\'t possible to read the file: ' . $file);
-                $output->writeln('Exception: ' . $e->getMessage());
-
-                return CommandAlias::INVALID;
-            }
-
-            //Make sure directory is present
-            $directory = Str::beforeLast($file, '/');
-            if (!is_dir('tests/' . $directory)) {
-                mkdir('tests/' . $directory, 0777, true);
-            }
+            $fileContent = $this->getSourceCodeFile($file); //Exits if file can't be loaded
+            $this->sourceCodeService->touchTestDir($file);
+            $testFileName = Str::beforeLast($file, '.') . 'Test.' . Str::afterLast($file, '.');
+            $testDisk = Storage::disk('test');
+            $testDisk->put($testFileName, '');
         }
 
         //Concatenate question and file name
@@ -70,7 +71,36 @@ class OpenAIRespond extends Command
         $token = config('services.openai_api_key');
 
         $client = app()->make(Client::class);
-        $data = [
+        $this->requestOpenAI($token, $str, $url, $client, $testDisk ?? null, $testFileName ?? null); //Exits if GuzzleException occurs
+
+        return CommandAlias::SUCCESS;
+    }
+
+    /**
+     * @param bool|array|string $file
+     * @return string
+     */
+    private function getSourceCodeFile(bool|array|string $file): string
+    {
+        try {
+            $fileContent = $this->sourceCodeService->getSourceCodeFile($file);
+        } catch (FilesystemException $e) {
+            $this->consoleOutput->writeln('It wasn\'t possible to read the file: ' . $file);
+            $this->consoleOutput->writeln('Exception: ' . $e->getMessage());
+            exit(CommandAlias::INVALID);
+        }
+
+        return $fileContent;
+    }
+
+    /**
+     * @param mixed $token
+     * @param string $str
+     * @return array
+     */
+    private function buildRequestData(mixed $token, string $str): array
+    {
+        return [
             'debug' => false,
             'stream' => true,
             'headers' => ['Authorization' => 'Bearer ' . $token],
@@ -85,30 +115,25 @@ class OpenAIRespond extends Command
                 'stream' => true,
             ],
         ];
+    }
+
+    private function requestOpenAI(string $token, string $str, string $url, Client $client, FilesystemAdapter $testDisk = null, string $testFileName = null): void
+    {
+        $data = $this->buildRequestData($token, $str);
         try {
             $response = $client->post($url, $data);
             $handle = $response->getBody()->detach();
 
             stream_filter_append($handle, 'remove_all_the_open_a_i_noise_from_stream_filter');
 
-            $testDisk = Storage::disk('test');
-
-            $testFileName = Str::beforeLast($file, '.') . 'Test.' . Str::afterLast($file, '.');
-
-            //Clear file content
-            $testDisk->put($testFileName, '');
-
             while ($binary = fread($handle, 8)) {
-                $output->write($binary);
-                $testDisk->append($testFileName, $binary, '');
+                $this->consoleOutput->write($binary);
+                $testDisk?->append($testFileName, $binary, '');
             }
         } catch (GuzzleException $e) {
-            $output->writeln('An error occurred while sending post request to: ' . $url);
-            $output->writeln('Code: ' . $e->getCode() . ' Message: ' . $e->getMessage());
-
-            return CommandAlias::FAILURE;
+            $this->consoleOutput->writeln('An error occurred while sending post request to: ' . $url);
+            $this->consoleOutput->writeln('Code: ' . $e->getCode() . ' Message: ' . $e->getMessage());
+            exit(CommandAlias::FAILURE);
         }
-
-        return CommandAlias::SUCCESS;
     }
 }
---
+++ app/Services/SourceCodeService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use League\Flysystem\FilesystemException;
+
+class SourceCodeService
+{
+    /**
+     * @param string $file
+     * @return string
+     * @throws FilesystemException
+     */
+    public function getSourceCodeFile(string $file): string
+    {
+        return Storage::disk('src')->read($file);
+    }
+
+    public function touchTestDir(string $file) : void
+    {
+        //Make sure directory is present
+        $directory = Str::beforeLast($file, '/');
+        if (!is_dir('tests/' . $directory)) {
+            mkdir('tests/' . $directory, 0777, true);
+        }
+    }
+}
