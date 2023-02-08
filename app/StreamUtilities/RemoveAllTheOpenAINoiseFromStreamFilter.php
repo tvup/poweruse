f097dad2f8e1652fefc339f000e820e6108edd97
Torben Evald Hansen <tvupper@gmail.com>
2023-02-08 15:34:59 +0100

    Appends some data about the context of the stream

--- app/StreamUtilities/RemoveAllTheOpenAINoiseFromStreamFilter.php
+++ app/StreamUtilities/RemoveAllTheOpenAINoiseFromStreamFilter.php
@@ -10,7 +10,17 @@ class RemoveAllTheOpenAINoiseFromStreamFilter extends php_user_filter
     public function filter($in, $out, &$consumed, $closing): int
     {
         while ($bucket = stream_bucket_make_writeable($in)) {
-            $bucket->data = $this->transform($bucket->data);
+            list($text, $choicesCount, $finishReason) = $this->transform($bucket->data);
+            //If choiceCount and finishReason are both null, we are beyond end of stream
+            //If choiceCount is different from 1 or finishReason has a value, we have reached end of stream, and we're
+            //appending some data from the context.
+            if (null == $choicesCount && null == $finishReason) {
+                $bucket->data = '';
+            } elseif ($choicesCount != 1 || $finishReason != null) {
+                $bucket->data = $text . json_encode(['choicesCount'=>$choicesCount, 'finishReason'=>$finishReason]);
+            } else {
+                $bucket->data = $text;
+            }
             $consumed += $bucket->datalen;
             stream_bucket_append($out, $bucket);
         }
@@ -18,15 +28,17 @@ class RemoveAllTheOpenAINoiseFromStreamFilter extends php_user_filter
         return PSFS_PASS_ON;
     }
 
-    private function transform(string $in): string
+    private function transform(string $in): array
     {
         $out = '';
         if ($json = json_decode(trim(Str::substr($in, 6)))) {
             foreach ($json->choices as $choice) {
                 $out .= $choice->text;
+                $finishReason = $choice->finish_reason;
             }
+            $choicesCount = count($json->choices);
         }
 
-        return $out;
+        return [$out, $choicesCount ?? null, $finishReason ?? null];
     }
 }
