bfacf08f5de27d2432390094288dc9fe679986cf
Torben Evald Hanen <contact@torbenit.dk>
2024-03-24 04:37:48 +0100

    Simplify Workbox setup and remove checks

--- resources/js/sw.js
+++ resources/js/sw.js
@@ -1,19 +1,13 @@
 // Importér Workbox-biblioteker (kun nødvendigt, hvis du bruger Workbox i en standalone SW uden Vite-plugin).
 // importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
 
-// Check om Workbox blev indlæst korrekt.
-if (workbox) {
-    console.log(`Yay! Workbox is loaded 🎉`);
+// Her bruger du self.__WB_MANIFEST, normalt sammen med Workbox's precacheAndRoute metode.
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
-    // Her bruger du self.__WB_MANIFEST, normalt sammen med Workbox's precacheAndRoute metode.
-    workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+// Tilføj dine egne event listeners og caching strategier...
+self.addEventListener('fetch', event => {
+    // Anvende strategier for caching eller returnere svar på fetch events
+});
 
-    // Tilføj dine egne event listeners og caching strategier...
-    self.addEventListener('fetch', event => {
-        // Anvende strategier for caching eller returnere svar på fetch events
-    });
-} else {
-    console.log(`Boo! Workbox didn't load 😬`);
-}
 
 // Dine egne event listeners og custom logik kan følge her...
