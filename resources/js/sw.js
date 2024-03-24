e90850bbe703c6762620b5f64aea5d3a8d032aa5
Torben Evald Hanen <contact@torbenit.dk>
2024-03-24 04:40:40 +0100

    Enable Workbox import in service worker

--- resources/js/sw.js
+++ resources/js/sw.js
@@ -1,5 +1,5 @@
 // Importér Workbox-biblioteker (kun nødvendigt, hvis du bruger Workbox i en standalone SW uden Vite-plugin).
-// importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
 
 // Her bruger du self.__WB_MANIFEST, normalt sammen med Workbox's precacheAndRoute metode.
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
