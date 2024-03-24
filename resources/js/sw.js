391c60bb0f43f6cc53ba06cae18c5c6e91a4e440
Torben Evald Hanen <contact@torbenit.dk>
2024-03-24 04:33:44 +0100

    Update Workbox setup and fetch handler

--- resources/js/sw.js
+++ resources/js/sw.js
@@ -1,12 +1,19 @@
-self.__WB_MANIFEST;
+// Importér Workbox-biblioteker (kun nødvendigt, hvis du bruger Workbox i en standalone SW uden Vite-plugin).
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
 
-// Workbox precaching. Oprette en selv-invokerende funktion:
-self.addEventListener('install', event => {
-    event.waitUntil(
-        // Precaching logik
-    );
-});
+// Check om Workbox blev indlæst korrekt.
+if (workbox) {
+    console.log(`Yay! Workbox is loaded 🎉`);
 
-self.addEventListener('fetch', event => {
-    // Anvende strategier for caching eller returnere svar på fetch events
-});
+    // Her bruger du self.__WB_MANIFEST, normalt sammen med Workbox's precacheAndRoute metode.
+    workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
+    // Tilføj dine egne event listeners og caching strategier...
+    self.addEventListener('fetch', event => {
+        // Anvende strategier for caching eller returnere svar på fetch events
+    });
+} else {
+    console.log(`Boo! Workbox didn't load 😬`);
+}
+
+// Dine egne event listeners og custom logik kan følge her...
