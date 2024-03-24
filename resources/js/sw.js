82cd102819aa8baa38a98d84df049ea11b695858
Torben Evald Hanen <contact@torbenit.dk>
2024-03-24 08:30:40 +0100

    Update .gitignore and streamline sw.js

--- resources/js/sw.js
+++ resources/js/sw.js
@@ -1,18 +1,10 @@
-// Importér Workbox-biblioteker (kun nødvendigt, hvis du bruger Workbox i en standalone SW uden Vite-plugin).
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
 
 if (workbox) {
     console.log(`Yay! Workbox is loaded 🎉`);
 
-    // Her bruger du self.__WB_MANIFEST, normalt sammen med Workbox's precacheAndRoute metode.
     workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
-
-
-
 
 } else {
     console.log(`Boo! Workbox didn't load 😬`);
 }
-
-
-// Dine egne event listeners og custom logik kan følge her...
