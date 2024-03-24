ba96f2ff20e8621e16efda34209b770572b7ae75
Torben Evald Hanen <contact@torbenit.dk>
2024-03-24 12:04:45 +0100

    Remove console logs from service worker

--- resources/js/sw.js
+++ resources/js/sw.js
@@ -1,10 +1,5 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
 
 if (workbox) {
-    console.log(`Yay! Workbox is loaded 🎉`);
-
     workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
-
-} else {
-    console.log(`Boo! Workbox didn't load 😬`);
 }
