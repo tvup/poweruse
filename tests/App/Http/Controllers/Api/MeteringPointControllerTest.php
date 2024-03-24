3c2dc8673e28fbfa105552c1e46a14a7864a8ff4
Torben Evald Hanen <contact@torbenit.dk>
2024-03-24 21:28:02 +0100

    Add MeteringPointFactory and update tests

--- tests/App/Http/Controllers/Api/MeteringPointControllerTest.php
+++ tests/App/Http/Controllers/Api/MeteringPointControllerTest.php
@@ -1,39 +1,46 @@
 <?php
 
+namespace Tests\App\Http\Controllers\Api;
+
 use App\Http\Controllers\Api\MeteringPointController;
 use App\Http\Requests\StoreMeteringPointRequest;
 use App\Http\Requests\UpdateMeteringPointRequest;
 use App\Models\MeteringPoint;
 use App\Models\User;
 use App\Services\GetMeteringData;
-use Illuminate\Http\Response;
+use Mockery;
+use PHPUnit\Framework\MockObject\Exception;
 use Symfony\Component\HttpFoundation\HeaderBag;
+use Symfony\Component\HttpFoundation\ParameterBag;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 use Tests\TestCase;
 
 class MeteringPointControllerTest extends TestCase
 {
-    private GetMeteringData $meteringDataService;
 
     private MeteringPointController $meteringPointController;
 
+    /**
+     * @throws Exception
+     */
     protected function setUp(): void
     {
         parent::setUp();
-        $this->meteringDataService = $this->createMock(GetMeteringData::class);
-        $this->meteringPointController = new MeteringPointController($this->meteringDataService);
+        $meteringDataService = $this->createMock(GetMeteringData::class);
+        $this->meteringPointController = new MeteringPointController($meteringDataService);
     }
 
-    public function testIndex()
+    public function testIndex() : void
     {
         $response = $this->meteringPointController->index();
-        $this->assertEquals($response->getStatusCode(), Response::HTTP_OK);
+        $this->assertEquals(ResponseAlias::HTTP_OK, $response->getStatusCode());
     }
 
-    public function testStore()
+    public function testStore() : void
     {
         $user = User::factory()->create();
         $this->actingAs($user, 'api');
-        $storeMeteringPointRequest = \Mockery::mock(StoreMeteringPointRequest::class)->makePartial();
+        $storeMeteringPointRequest = Mockery::mock(StoreMeteringPointRequest::class)->makePartial();
         $array = ['metering_point_id' => 'dsfdsfds', 'type_of_mp' => '1', 'settlement_method' => 'D02', 'meter_number' => '1234', 'meter_reading_occurrence' => 'PYTH1', 'balance_supplier_name' => 'supplier', 'street_code' => '123', 'street_name' => 'Omvejen', 'building_number' => '12', 'city_name' => 'Ørsted', 'municipality_code' => '123', 'hasRelation' => 0];
         $storeMeteringPointRequest->shouldReceive('all')
             ->andReturn($array);
@@ -43,37 +50,44 @@ class MeteringPointControllerTest extends TestCase
         $this->assertIsArray($response);
     }
 
-    public function testShow()
+    /**
+     * @throws Exception
+     */
+    public function testShow() : void
     {
         $meteringPoint = $this->createMock(MeteringPoint::class);
         $response = $this->meteringPointController->show($meteringPoint);
-        $this->assertEquals($response->getStatusCode(), Response::HTTP_OK);
+        $this->assertEquals(ResponseAlias::HTTP_OK, $response->getStatusCode());
     }
 
-    public function testUpdate()
+    public function testUpdate() : void
     {
-        $updateMeteringPointRequest = \Mockery::mock(UpdateMeteringPointRequest::class)->makePartial();
-        $array = ['metering_point_id' => 'dsfdsfds', 'type_of_mp' => '1', 'settlement_method' => 'D02', 'meter_number' => '1234', 'meter_reading_occurrence' => 'PYTH1', 'balance_supplier_name' => 'supplier', 'street_code' => '123', 'street_name' => 'Omvejen', 'building_number' => '12', 'city_name' => 'Ørsted', 'municipality_code' => '123', 'hasRelation' => 0];
+        $meteringPoint = MeteringPoint::factory()->create(['metering_point_id' => 'dsfdsfds', 'type_of_mp' => '1', 'settlement_method' => 'D02', 'meter_number' => '1234', 'meter_reading_occurrence' => 'PYTH1', 'balance_supplier_name' => 'supplier', 'street_code' => '123', 'street_name' => 'Omvejen', 'building_number' => '12', 'city_name' => 'Ørsted', 'municipality_code' => '123', 'hasRelation' => 0]);
+
+        $updateMeteringPointRequest = Mockery::mock(UpdateMeteringPointRequest::class)->makePartial();
+
         $updateMeteringPointRequest->shouldReceive('all')
-            ->andReturn($array);
+            ->andReturn($meteringPoint->toArray());
         $updateMeteringPointRequest->shouldReceive('validated')
-            ->andReturn($array);
-        $parameterBag = \Mockery::mock(\Symfony\Component\HttpFoundation\ParameterBag::class)->makePartial();
+            ->andReturn($meteringPoint->toArray());
+        $parameterBag = Mockery::mock(ParameterBag::class)->makePartial();
         $parameterBag->shouldReceive('get')
             ->andReturn(true);
         $updateMeteringPointRequest->attributes = $parameterBag;
-        $headerBag = \Mockery::mock(HeaderBag::class)->makePartial();
+        $headerBag = Mockery::mock(HeaderBag::class)->makePartial();
         $updateMeteringPointRequest->headers = $headerBag;
 
-        $meteringPoint = new MeteringPoint(['metering_point_id' => 'dsfdsfds', 'type_of_mp' => '1', 'settlement_method' => 'D02', 'meter_number' => '1234', 'meter_reading_occurrence' => 'PYTH1', 'balance_supplier_name' => 'supplier', 'street_code' => '123', 'street_name' => 'Omvejen', 'building_number' => '12', 'city_name' => 'Ørsted', 'municipality_code' => '123', 'hasRelation' => 0]);
         $response = $this->meteringPointController->update($updateMeteringPointRequest, $meteringPoint);
-        $this->assertEquals($response->getStatusCode(), Response::HTTP_OK);
+        $this->assertEquals(ResponseAlias::HTTP_OK, $response->getStatusCode());
     }
 
-    public function testDestroy()
+    /**
+     * @throws Exception
+     */
+    public function testDestroy() : void
     {
         $meteringPoint = $this->createMock(MeteringPoint::class);
         $response = $this->meteringPointController->destroy($meteringPoint);
-        $this->assertEquals($response->getStatusCode(), Response::HTTP_NO_CONTENT);
+        $this->assertEquals(ResponseAlias::HTTP_NO_CONTENT, $response->getStatusCode());
     }
 }
